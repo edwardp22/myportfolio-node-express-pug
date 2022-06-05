1cbc764403b7d6ffba3648e298db876c15150f01
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-06-05 09:43:44 -0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# My Portfolio
+
+Portfolio project showcasing my personal projects.
+
+
+## Screenshots
+
+![App Screenshot](https://i.ibb.co/ctDfSLC/myportfolio1.png)
+![App Screenshot](https://i.ibb.co/0ZtfbBg/myportfolio2.png)
+
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const data = require("./data.json");
+const app = express();
+
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { projects: data.projects });
+});
+
+app.get("/project/:id", (req, res, next) => {
+  const project = data.projects.find(
+    (project) => project.id === +req.params.id
+  );
+
+  if (project) {
+    res.render("project", { project });
+  } else {
+    next();
+  }
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { message: "Page not found" });
+});
+
+app.listen(3000, () => console.log("Running on localhost 3000"));
