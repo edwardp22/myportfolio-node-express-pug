7719ef7e757f5b73728732d1289e07d6201f6499
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-06-10 14:37:30 -0700

    technologies and filters

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
-const data = require("./data.json");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+const data = require("./data.json");
+// Extracting a list of all technologies to be used in the filter section
+const technologiesList = [];
+data.projects.forEach((project) => {
+  project.technologies.forEach((tech) => {
+    if (!technologiesList.some((techL) => techL === tech)) {
+      technologiesList.push(tech);
+    }
+  });
+});
 
 app.set("view engine", "pug");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index", { projects: data.projects });
+  res.render("index", { projects: data.projects, technologiesList });
+});
+
+app.get("/:type", (req, res) => {
+  const projects = data.projects.filter((project) =>
+    project.technologies.some(
+      (tech) => tech.toUpperCase() === req.params.type.toUpperCase()
+    )
+  );
+
+  res.render("index", { projects: projects, technologiesList });
 });
 
 app.get("/project/:id", (req, res, next) => {
